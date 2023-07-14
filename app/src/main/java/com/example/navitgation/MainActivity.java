5bff27df4be04f3e06b051cc4c6a9a343364166c
LeonardoXavier1 <leonardomx500@gmail.com>
2023-07-14 00:16:44 -0300

    certo

--- app/src/main/java/com/example/navitgation/MainActivity.java
+++ app/src/main/java/com/example/navitgation/MainActivity.java
@@ -5,8 +5,10 @@ import static java.security.AccessController.getContext;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.NotificationCompat;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -17,6 +19,8 @@ public class MainActivity extends AppCompatActivity {
     private EditText editUser;
     private EditText editPassword;
 
+    private Button buttonLogin;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setContentView(R.layout.activity_main);
@@ -24,6 +28,21 @@ public class MainActivity extends AppCompatActivity {
 
         editUser = findViewById(R.id.editUser);
         editPassword = findViewById(R.id.editPassword);
+
+        iniciarComponentes();
+
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, LoginRealizado.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    private void iniciarComponentes(){
+        buttonLogin=findViewById(R.id.buttonLogin);
     }
 
     public void loginUser(View view){
@@ -34,6 +53,8 @@ public class MainActivity extends AppCompatActivity {
             System.out.println("passou aqui");
         } else {
             System.out.println("erro");
+            System.out.println(user);
+            System.out.println(password);
         }
     }
 }
